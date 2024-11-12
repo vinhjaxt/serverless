a401a7f2badd5d0f2bb88702167c275a2e6ed1ba
vinhjaxt <ngoc.lo.dang@gmail.com>
2024-11-12 14:07:38 +0700

    Update index.js

--- cloudflare-serverless/src/index.js
+++ cloudflare-serverless/src/index.js
@@ -9,7 +9,8 @@ let PGSQL_URL = 'postgresql://postgres.xxx:xxx@aws-0-ap-southeast-1.pooler.supab
 async function handler(req, env, ctx) {
   try {
     const sql = postgres(PGSQL_URL, {
-      max: 30,
+      // max: 6, // xata: https://xata.io/docs/concepts/pricing#concurrency-limit
+      max: 30, // supbabase: https://supabase.com/docs/guides/platform/compute-and-disk#postgres-replication-slots-wal-senders-and-connections
       idle_timeout: 15000,
       connect_timeout: 7000,
     })
