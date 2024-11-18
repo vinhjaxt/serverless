1f2f48d40dcf93666c02851e492547de212d6107
vinhjaxt <ngoc.lo.dang@gmail.com>
2024-11-18 06:55:42 +0000

    Update main.ts for Aiven connection

--- main.ts
+++ main.ts
@@ -9,6 +9,35 @@ const sql = postgres(PGSQL_URL, {
   max: 30, // supabase: https://supabase.com/docs/guides/platform/compute-and-disk#postgres-replication-slots-wal-senders-and-connections
   idle_timeout: 15000,
   connect_timeout: 7000,
+  /* Aiven custom CA
+    ssl: {
+        caCerts: [`-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUQIi5+rjPukyUemd/4daTLOHSb5MwDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvY2MyOWYzNDItYjgwNC00MWYzLThjYTUtNTY2Nzk4MWJk
+YzIwIFByb2plY3QgQ0EwHhcNMjQxMTE4MDM1MjQ1WhcNMzQxMTE2MDM1MjQ1WjA6
+MTgwNgYDVQQDDC9jYzI5ZjM0Mi1iODA0LTQxZjMtOGNhNS01NjY3OTgxYmRjMjAg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAL+LxJaK
+X652kOwvF+T1d/qeR1Kuy4NhAlIrOdCtYZhbJy8tPH31qXiKvX6Kmv1SOQ9Rwsap
+RPtLAbto9uJDHmOksDuV03/ji05615e6qOKFX40gL70zifHcEpzmGial2NhOZFvI
+/hj6a6Ajiz10J8948nqvLHGGKDjyfoZJjBlemg59nphharU+gdZvVLersrogntkQ
+yEECWRR8gX7u4VRB5CbQR/9dU9cHAjDpZQL96HFjtQzS0MoWqARaOlvJVvp04xM1
+F06//mVMo7j8rZI1Soqfr4a3Aq5eBk75Z+/x5B0o4QU/BpXMgh84H84hbB3YO+vB
+Gfc+c3bRumZ+a/VlocU1s9GwKRyt0yknRn4dx6w7+fpVuih86IASw4BYHUGdlArt
+RPjnvXMcjDOEmqrT20ypKwMeyjJvjTQuj4zldzWHleCJKWyJcLOmX0gHq2RaR33E
+auAqMOP4fCWfl/3Zqb4uSn5SAXdFYqtH8ZsoHLCcLvH8A72vQpotC3AciwIDAQAB
+oz8wPTAdBgNVHQ4EFgQUsNVr6fbh8sUiNwOkrlcQ+S8k0+MwDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBADCqqIlkrYvuT7lS
+F3xiMtCjMf+jKmP55PNC1ZotFe1wPAud6Lm5XGqbevPbR4d4cLwgOQgWy8lR+H+O
+LB74bEjBsnZDD5A8OgMQAq5a3O7D5h1Z3u15x65CCYrYE9KHiTSWWisML0QEJzWW
+eogJ9lHUPlQUG0Ne0QMB/Zt3nd3e7vcQClqz0DVVn+o3ludf/1P3IZNIT/ioAb+g
+A4e1vaVB34J8EJZuA+8KA7Fdayy2jd9RukpWgJksenV2xMiKTA9Eoh/DTjTwOvGs
+XjyaQDBRDXC/aTTQ9QjMlgEnApdUN5icyZa319PdUFeoTGnXYBPJuGRXjYxQGfgv
+/bioegKZA1J+OSc9OBLl7heq6vpKYikPYV5xVqrmy4AwUUhQa6x8aSzfgK0nPZ/Z
+u/5fOcsqX1qkrte9sr/KMckB/2e//5JF8qdiHZlu9J7t904gKKH1/NKPNXWaVtny
+oqdu2OA6clPjvqj0bm5zca5MP2OlIPtiKE76KrV78WCWKn8Bcg==
+-----END CERTIFICATE-----`],
+    },
+  // */
 })
 
 const handler = async (req: Request) => {
